0a6f2937267342ea40a78d1e981ce59bdebaa285
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2022-02-14 16:50:29 +0530

    Add files via upload

---
+++ movie_rec_system_1.py
@@ -0,0 +1,41 @@
+dataset = {
+    "action" : ["matrix","batman","superman","dabang","avengers","thor",
+                "hulk","krrish","ironman",'kgf'],
+    "comedy" : ["the mask","dhamaal","bala","housefull","golmaal",
+                "hera pheri","golmaal 2","dhamaal 2","hera pheri 2"],
+    "drama" : ["dabang","krrish","hera pheri","bala","kahani","batla house",
+               "kgf","zero","sultan"],
+    "thriller" : ["kahani","kgf","batla house","madras cafe","uri",
+                  "raw","lucy","the ring","oculus"],
+    "horror" : ["oculus","the ring","it","the ring 2","evil dead",
+                "conjuring","conjuring 2","bhootnath","aatma"]
+}
+
+user = {"kgf", "zero", "superman", "avengers", "batman", "evil dead", "sultan",
+          "golmaal", "krrish"}
+
+#scores = {"action":0.33, "comedy":20...} using jaccard distance formula
+#find the genre with max score
+#recommend movies from that genre which has max score and user has not watched those movies yet...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
