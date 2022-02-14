607712d753b68fa9c15dd171f18b9ff48417379a
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2022-02-14 16:22:20 +0530

    Add files via upload

--- dictionary_exercise.py
+++ dictionary_exercise.py
@@ -11,11 +11,52 @@ products = [
     {"p_name":"Asus Zenbook","brand":"Asus","Category":"Laptop","price":88000},
     ]
 
-toSearch = input("Enter product : ")
+def sortData(x):
+    return x["price"]
 
-'''
-- user can enter product name, product brand or category
-- now store the data in a different list (searchResults = [])
-- ask user if he wants to sort data based on price
-- ask the order of sorting
-'''
+while True:
+    searchResults = []
+    toSearch = input("Enter product : ")
+    toSearch = toSearch.lower()
+
+    '''
+    - user can enter product name, product brand or category
+    - now store the data in a different list (searchResults = [])
+    - ask user if he wants to sort data based on price
+    - ask the order of sorting
+    '''
+
+    for i in range(len(products)):
+        '''
+        condition_1 = products[i]['p_name'].lower() == toSearch
+        condition_2 = products[i]['brand'].lower() == toSearch
+        condition_3 = products[i]['Category'].lower() == toSearch
+        '''
+        condition_1 = toSearch in products[i]['p_name'].lower()
+        condition_2 = toSearch in products[i]['brand'].lower()
+        condition_3 = toSearch in products[i]['Category'].lower()
+        if condition_1 or condition_2 or condition_3:
+            print(products[i])
+            searchResults.append(products[i])
+
+    choice = input("You want to sort data based on price : [Y/N] ")
+    choice = choice.lower()
+    if choice == "y":
+        print("1. Low to High")
+        print("2. High to Low")
+        sortChoice = input(": ")
+        if sortChoice == "1":
+            sortedOutput = sorted(searchResults, key=sortData)
+            print(sortedOutput)
+        else:
+            sortedOutput = sorted(searchResults, key=sortData, reverse=True)
+            print(sortedOutput)
+
+
+
+
+
+
+
+
+
