54c18d0f11e5cff6e73814b66d07529f6f8c2693
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2022-02-15 16:53:48 +0530

    Add files via upload

---
+++ plagiarism_checker.py
@@ -0,0 +1,35 @@
+text_1 = """
+Python is a high-level programming language designed to be easy to read and
+simple to implement. It is open source, which means it is free to use, even
+for commercial applications. Python can run on Mac, Windows, and Unix systems
+and has also been ported to Java and .NET virtual machines.
+Python is considered a scripting language, like Ruby or Perl and is often
+used for creating Web applications and dynamic Web content.
+"""
+
+text_2 = """
+Python is an interpreted, object-oriented, high-level programming language
+with dynamic semantics developed by Guido van Rossum. It was originally released
+in 1991. Designed to be easy as well as fun, the name "Python" is a nod to the
+British comedy group Monty Python. Python has a reputation as a beginner-friendly
+language, replacing Java as the most widely used introductory language because
+it handles much of the complexity for the user, allowing beginners to focus on
+fully grasping programming concepts rather than minute details.
+"""
+
+'''
+Use jaccard distance to find similarity b/w 2 text
+Steps
+1 - Split text - Tokenization
+2 - Remove Stopwords = is, are, that, the, who, has, been, it....
+3 - Remove Punctuation = ,/.;'[]!@#$%^&*()_+-=
+4 - Type cast data into set
+5 - Find Similarity using jaccard distance
+'''
+
+
+
+
+
+
+
